e5eaa8e954d125395254a9b8beaa097054470f59
Ajay Sankar <ajay0196@gmail.com>
2020-07-14 19:34:02 -0700

    Added NavBar and styles..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World </h1>
+      <NavBar/>
     </div>
   );
 }
